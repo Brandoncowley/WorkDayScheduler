780ff15a3b3a426b1231648f90603bc7776ba31d
Brandon Cowley <brandon.cowley@gmail.com>
2021-04-23 15:35:44 -0700

    Trying to make the HTML work, something isn't linking to JS

--- script.js
+++ script.js
@@ -5,3 +5,29 @@ var dateDiv = "#currentDay"
 
 $(dateDiv).text(dt.toLocaleString(DateTime.DATETIME_MED))
 console.log($(dateDiv).text())
+
+var textInput = document.querySelector("text");
+var saveButtonEl = $(".saveBtn");
+
+renderLastRegistered();
+
+function renderLastRegistered() {
+    var text = localStorage.getItem("text");
+
+    if (!text) {
+        return;
+    }
+
+    text.textContent = text;
+}
+
+saveButtonEl.on('click', function () {
+    preventDefault();
+
+    var text = document.querySelector("#text").value;
+
+    localStorage.setItem("text", text);
+    renderLastRegistered();
+
+
+})
