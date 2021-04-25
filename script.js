6066e33514c6c2392a9fc7b3d5aaebf43d39e8a8
Brandon Cowley <brandon.cowley@gmail.com>
2021-04-25 11:25:09 -0700

    JS working with correct loop for buttons

--- script.js
+++ script.js
@@ -1,33 +1,58 @@
-var DateTime = luxon.DateTime
-var dt = DateTime.now()
-console.log(dt.toLocaleString(DateTime.DATETIME_MED))
-var dateDiv = "#currentDay"
+// moment might be outdated, check out Luxon
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
 
-$(dateDiv).text(dt.toLocaleString(DateTime.DATETIME_MED))
-console.log($(dateDiv).text())
+$(document).ready(function () {
+    // saveBtn click listener 
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
 
-var textInput = document.querySelector("text");
-var saveButtonEl = $(".saveBtn");
+        // Save text in local storage
+        localStorage.setItem(time, text);
+    })
+   
+    function timeTracker() {
+        //get current number of hours.
+        var timeNow = moment().hour();
 
-renderLastRegistered();
+        // loop over time blocks
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
-function renderLastRegistered() {
-    var text = localStorage.getItem("text");
+            // To check the time and add the classes for background indicators
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
 
-    if (!text) {
-        return;
+            }
+        })
     }
 
-    text.textContent = text;
-}
+    // Get item from local storage if any
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour18 .description").val(localStorage.getItem("hour18"));
 
-saveButtonEl.on('click', function () {
-    preventDefault();
-
-    var text = document.querySelector("#text").value;
-
-    localStorage.setItem("text", text);
-    renderLastRegistered();
-
-
+    timeTracker();
 })
